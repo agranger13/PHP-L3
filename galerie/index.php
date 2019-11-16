d1b36e8976d64d680a6c28de360b8ce2b9511a4e
root <granger.alexis13@gmail.com>
2019-11-16 17:23:28 +0100

    16/11/2019

--- galerie/index.php
+++ galerie/index.php
@@ -1,8 +1,57 @@
 <?php
+    $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
     $images = scandir ("Images");
-    foreach($images as $i){
-        if($i != "." || $i != ".."){
-            echo $i;
-        }
-    }
+
+    $query = new MongoDB\Driver\Query([]);
+    $albums = $manager->executeQuery('galerie.albums',$query);
+    $albums = $albums->toArray();
+
 ?>
+
+
+<head>
+        <meta charset="UTF-8">
+        <title> Galerie </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <h1 class="text-center"> Ma galerie </h1>
+            <div class="row h-75">
+
+                <!-- Options -->
+                <form class="col-4 bg-dark" method="post">
+                    <div class="row">
+                        <div class="col">
+                            <p class="text-light">Images</p>
+                            <div class="list-group overflow-auto">
+                                <?php
+                                    foreach($images as $img){
+                                        if($img != "." && $img != "..")
+                                            echo "<button class='list-group-item list-group-item-action' type='sumbit' name='image' value='".$img."'>".$img."</button>";
+                                    }
+                                ?>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <p class="text-light">Albums</p>
+                            <ul class="list-group overflow-auto">
+                                <?php
+                                     echo "<button class='list-group-item list-group-item-action' type='sumbit' name='album' value='all'>Toute les images</button>";
+                                    foreach($albums as $alb){
+                                        echo "<button class='list-group-item list-group-item-action' type='sumbit' name='album' value='".$alb."'>".$alb."</button>";
+                                    }
+                                ?>
+                            </ul>
+                        </div>
+                    </div>
+                </form>
+
+                <!-- Defilement images -->
+                <div class="col bg-light">
+                
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- planning/index.php
+++ planning/index.php
@@ -30,7 +30,6 @@
     
                 if($_POST['affect'.$i] != $actifUser[0]->userId){
                     $bulk = new MongoDB\Driver\BulkWrite;
-                    echo $affect->date."    ";
                     $bulk->update(
                         ['date' => $affect->date],
                         ['$set' => ['userId' => (int)$_POST['affect'.$i]]],
@@ -66,71 +65,73 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        <h1 class="text-center"> Planning des corvées  <?php echo $annee ?>  </h1>
-        <form action='' method="post" class="text-center">
-            <label>Année : </label>
-            <select name="annee" autocomplete='off'>
-                <option value="2018" <?php 
-                    if(!empty($_POST['annee']) && $_POST['annee']=="2018"){ 
-                        echo "selected='select'" ;
-                    } ?>>2018</option>
-                <option value="2019" <?php 
-                    if(!empty($_POST['annee']) && $_POST['annee']=="2019"){ 
-                        echo "selected='select'" ;
-                    } ?> >2019</option>
-                <option value="2020" <?php 
-                    if(!empty($_POST['annee']) && $_POST['annee']=="2020"){ 
-                        echo "selected='select'" ;
-                    } ?>>2020</option>
-            </select>
+        <div class="container">
+            <h1 class="text-center"> Planning des corvées  <?php echo $annee ?>  </h1>
+            <form action='' method="post" class="text-center">
+                <label>Année : </label>
+                <select name="annee" autocomplete='off'>
+                    <option value="2018" <?php 
+                        if(!empty($_POST['annee']) && $_POST['annee']=="2018"){ 
+                            echo "selected='select'" ;
+                        } ?>>2018</option>
+                    <option value="2019" <?php 
+                        if(!empty($_POST['annee']) && $_POST['annee']=="2019"){ 
+                            echo "selected='select'" ;
+                        } ?> >2019</option>
+                    <option value="2020" <?php 
+                        if(!empty($_POST['annee']) && $_POST['annee']=="2020"){ 
+                            echo "selected='select'" ;
+                        } ?>>2020</option>
+                </select>
 
-            <button type="submit" class="btn btn-primary" value="Changer" name="action"> Changer l'année </button>
+                <button type="submit" class="btn btn-primary" value="Changer" name="action"> Changer l'année </button>
 
-            <table class="table table-striped">
-                <?php
-                
-                    foreach($affectation as $i=>$affect){
+                <table class="table table-striped">
+                    <?php
+                    
+                        foreach($affectation as $i=>$affect){
+                            
+                            if($i%4 == 0){
+                                echo "<tr>";
+                            }
+
+                            echo "<td>".$affect->date;
+                            echo "<select  autocomplete='off' name='affect".$i."'>";
+
+                            $query = new MongoDB\Driver\Query(["userId"=>$affect->userId]);
+                            $actifUser = $manager->executeQuery('planning.users',$query);
+                            $actifUser = $actifUser->toArray();
                         
-                        if($i%4 == 0){
-                            echo "<tr>";
+                            foreach($users as $user){
+                                if($actifUser[0]->userId != $user->userId){
+                                    echo "<option value=".$user->userId." >".$user->name."</option>";
+                                }else {
+                                    echo "<option value=".$actifUser[0]->userId." selected='select'>".$actifUser[0]->name."</option>";
+                                    $stat[$actifUser[0]->name] ++;
+                                }                  
+                            }
+
+                            echo "</select>";
+                            echo "</td>";
+                            
+
+                            if($i%4 == 3){
+                                echo "</tr>";
+                            }
                         }
+                    ?>
+                </table>
+                <button type="submit" class="btn btn-primary" value="valider" name="action"> Valider le planning </button>
+            </form>
 
-                        echo "<td>".$affect->date;
-                        echo "<select  autocomplete='off' name='affect".$i."'>";
-
-                        $query = new MongoDB\Driver\Query(["userId"=>$affect->userId]);
-                        $actifUser = $manager->executeQuery('planning.users',$query);
-                        $actifUser = $actifUser->toArray();
-                     
-                        foreach($users as $user){
-                            if($actifUser[0]->userId != $user->userId){
-                                echo "<option value=".$user->userId." >".$user->name."</option>";
-                            }else {
-                                echo "<option value=".$actifUser[0]->userId." selected='select'>".$actifUser[0]->name."</option>";
-                                $stat[$actifUser[0]->name] ++;
-                            }                  
-                        }
-
-                        echo "</select>";
-                        echo "</td>";
-                        
-
-                        if($i%4 == 3){
-                            echo "</tr>";
-                        }
-                    }
-                ?>
-            </table>
-            <button type="submit" class="btn btn-primary" value="valider" name="action"> Valider le planning </button>
-        </form>
-
-        <h3 class="text-center" >Statistiques des attributions des corvées en <?php echo $annee ?> </h3>
-        <?php
-            asort($stat);
-            foreach($stat as $user=>$count){
-                echo "<p class='ml-2'>".$user." : ".$count."</p>";
-            }
-        ?>
+            <h3 class="text-center" >Statistiques des attributions des corvées en <?php echo $annee ?> </h3>
+            <?php
+                asort($stat);
+                foreach($stat as $user=>$count){
+                    echo "<p class='ml-2'>".$user." : ".$count."</p>";
+                }
+            ?>
+        </div>
     </body>
 </html>
 
