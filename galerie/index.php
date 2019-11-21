02a8f6641360924227f88f1b347d854942465206
root <granger.alexis13@gmail.com>
2019-11-21 19:28:48 +0100

    Items checkables with JS

--- galerie/index.php
+++ galerie/index.php
@@ -8,42 +8,68 @@
 
 ?>
 
-
-<head>
+<html>
+    <head>
         <meta charset="UTF-8">
         <title> Galerie </title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
     </head>
     <body>
         <div class="container-fluid">
             <h1 class="text-center"> Ma galerie </h1>
-            <div class="row h-75">
+            <div class="row">
 
                 <!-- Options -->
                 <form class="col-4 bg-dark" method="post">
-                    <div class="row">
+                    <div class="row image-list">
                         <div class="col">
                             <p class="text-light">Images</p>
-                            <div class="list-group overflow-auto">
+                            <div class="list-group overflow-auto" id="list-img" role="tablist">
                                 <?php
                                     foreach($images as $img){
-                                        if($img != "." && $img != "..")
-                                            echo "<button class='list-group-item list-group-item-action' type='sumbit' name='image' value='".$img."'>".$img."</button>";
+                                        if($img != "." && $img != ".."){
+                                            echo "<a href='#' onclick='selectImages(this)' class='list-group-item list-group-item-action'>";
+                                            
+                                            echo "<input type='checkbox' name='image[]' value='".$img."'> ";
+                                            echo $img;
+
+                                            echo "</a>";
+                                        }
                                     }
                                 ?>
                             </div>
                         </div>
                         <div class="col">
                             <p class="text-light">Albums</p>
-                            <ul class="list-group overflow-auto">
+                            <ul class="list-group overflow-auto" id="list-galerie" role="tablist">
                                 <?php
-                                     echo "<button class='list-group-item list-group-item-action' type='sumbit' name='album' value='all'>Toute les images</button>";
+                                    echo "<a href='#' onclick='selectGalerie(this)' class='list-group-item list-group-item-action'>";
+                                    echo "<input type='checkbox' name='album' value='all'>";
+                                    echo "Toute les images";
+                                    echo "</a>";
                                     foreach($albums as $alb){
-                                        echo "<button class='list-group-item list-group-item-action' type='sumbit' name='album' value='".$alb."'>".$alb."</button>";
+                                        echo "<a href='#' onclick='selectGalerie(this)' class='list-group-item list-group-item-action'>";
+                                        echo "<input type='checkbox' name='album' value='".$alb."'>";
+                                        echo $alb;
+                                        echo "</a>";
                                     }
+                                    
+                                    
                                 ?>
                             </ul>
                         </div>
+                    </div>
+
+                    <div class="row justify-content-center" id="modifyCollect">
+                        <button type="submit" name="action" value="AddToCollect" class="col-3 btn btn-light">Ajouter</button>
+                        <button type="submit" name="action" value="RemoveFromCollect" class="col-3 btn btn-light">Retirer</button>
+                    </div>
+
+                    <div class="row justify-content-center"  id="createCollect">
+                        <input type="text" name="nameCollect" class="col-8" placeholder="Nom de la Collection">
+                        <button type="submit" name="action" value="CreateCollect" class="col-3 btn btn-light">Creer</button>
                     </div>
                 </form>
 
@@ -53,5 +79,32 @@
                 </div>
             </div>
         </div>
+
+        <script>
+            function selectImages (elem){
+                if(elem.classList.contains('active')){
+                    elem.classList.remove('active');
+                }else{
+                    elem.classList.add('active');
+                }
+            }
+
+            function selectGalerie (elem){
+                if(!elem.classList.contains('active')){
+                    elem.classList.add('active');
+                    listG = document.querySelectorAll("#list-galerie a");
+                    for(var g of listG){
+                        console.log(g);
+                        if(elem != g){
+                            g.childNodes[0].checked = false;
+                            g.classList.remove('active');
+                        }
+                            
+                    }
+                }else{
+                    elem.childNodes[0].checked = true;
+                }
+            }
+        </script>
     </body>
 </html>
